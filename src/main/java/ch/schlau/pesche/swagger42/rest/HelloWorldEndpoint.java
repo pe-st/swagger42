e5c9da7bc2a0d2c2b3c912d318cddba3961b356b
Peter Steiner <unistein+reinette@gmail.com>
2018-02-16 23:17:46 +0100

    Add swagger annotations
    swagger.json is generated as:
    
    {
      "swagger": "2.0",
      "info": {},
      "basePath": "/",
      "paths": {
        "/hello": {
          "get": {
            "summary": "Get the response",
            "description": "",
            "operationId": "doGet",
            "produces": [
              "text/plain"
            ],
            "parameters": [],
            "responses": {
              "default": {
                "description": "successful operation"
              }
            }
          }
        }
      }
    }

--- src/main/java/ch/schlau/pesche/swagger42/rest/HelloWorldEndpoint.java
+++ src/main/java/ch/schlau/pesche/swagger42/rest/HelloWorldEndpoint.java
@@ -1,18 +1,21 @@
 package ch.schlau.pesche.swagger42.rest;
 
-
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api
 @Path("/hello")
 public class HelloWorldEndpoint {
 
-	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Hello from WildFly Swarm!").build();
-	}
+    @ApiOperation("Get the response")
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok("Hello from WildFly Swarm!").build();
+    }
 }
